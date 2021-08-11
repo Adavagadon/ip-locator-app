1bf428d2a14fc32c68589f747c6742f99fa434e0
adavagadon <kiriushkindmitrii@gmail.com>
2021-08-12 00:55:52 +0300

    add media queries and error validation

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
 
   const handleOnChange = (e) => {
     const string = e.target.value;
-    setVal((string.match(/[\d.]{1,}/g)) || '');
+    setVal(string.match(/[\d.]{1,}/g) || '');
   }
 
   const handleOnSubmit = (e) => {
@@ -35,18 +35,22 @@ const App = () => {
     fetch(url)
       .then(json => json.json())
       .then((data) => {
-        console.log(data.location);
-        const newData = {
-          ip: data.ip, 
-          coords: [data.location.lat, data.location.lng], 
-          location: data.location.city + (data.location.postalCode ? ", " + data.location.postalCode : ''),
-          timezone: 'UTC ' + data.location.timezone, 
-          isp: data.isp, 
-          popup: data.ip + '\n' + data.location.city
+        setError('');
+        if(data.messages !== undefined) {
+          setError(data.messages);
+        } else {
+          const newData = {
+            ip: data.ip, 
+            coords: [data.location.lat, data.location.lng], 
+            location: data.location.city + (data.location.postalCode ? ", " + data.location.postalCode : '') || '-',
+            timezone: data.location.timezone ? 'UTC ' + data.location.timezone : '-', 
+            isp: data.isp, 
+            popup: data.ip + '\n' + data.location.city
+          }
+          setData(newData);
+          setVal('');
         }
-        setData(newData);
-      })
-      .finally(() => {setVal('')});
+      });
   }
 
   return (
@@ -55,20 +59,28 @@ const App = () => {
         <div className="wrapper">
           <h1>IP Address Tracker</h1>
           <form className="shadow" onSubmit={handleOnSubmit}>
-            {error ? <div className="error">{error}</div> : ''}
-            <input type="text" name="address" id="address" value={val} onChange={handleOnChange}/>
+            {error ? <div className="error shake">{error}</div> : ''}
+            <input type="text" name="address" id="address" value={val} onChange={handleOnChange} placeholder="Search for any IP address or domain" className={error ? 'error' : ''}/>
             <input type="submit" value=""/>
           </form>
           <section className="result shadow">
             <div className="wrapper">
-              <p className="result__name">ip address</p>
-              <p className="result__value">{data.ip}</p>
-              <p className="result__name">location</p>
-              <p className="result__value">{data.location}</p>
-              <p className="result__name">timezone</p>
-              <p className="result__value">{data.timezone}</p>
-              <p className="result__name">isp</p>
-              <p className="result__value">{data.isp}</p>
+              <div className="result__block">
+                <p className="result__name">ip address</p>
+                <p className="result__value">{data.ip}</p>
+              </div>
+              <div className="result__block">
+                <p className="result__name">location</p>
+                <p className="result__value">{data.location}</p>
+              </div>
+              <div className="result__block">
+                <p className="result__name">timezone</p>
+                <p className="result__value">{data.timezone}</p>
+              </div>
+              <div className="result__block">
+                <p className="result__name">isp</p>
+                <p className="result__value">{data.isp}</p>
+              </div>
             </div>
           </section>
         </div>
@@ -95,7 +107,7 @@ const App = () => {
 
 const Test = ({data}) => {
   const map = useMap();
-  map.flyTo(data.coords);
+  map.flyTo(data.coords, 14);
   return null;
 }
 
